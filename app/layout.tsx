51503e5226416a861ff993274174198d7dd3667c
Donny <donny.rendi@yahoo.co.id>
2023-12-19 10:56:12 +0700

    build: next theme dark mode added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -23,9 +24,16 @@ export default function RootLayout({
           inter.variable,
         )}
       >
-        <div id='web-wrapper' className='container'>
-          {children}
-        </div>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div id='web-wrapper' className='container'>
+            {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
