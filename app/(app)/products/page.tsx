5fb295dbe5e42c842b387f8bbc19f2ba288a4ce2
Donny <donny.rendi@yahoo.co.id>
2023-12-26 10:10:54 +0700

    build: products page

--- app/(app)/products/page.tsx
+++ app/(app)/products/page.tsx
@@ -1,25 +1,38 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { allProducts } from '@/data/data'
+import { Badge } from '@/components/ui/badge'
 
 export default function Page() {
   return (
     <>
-      <h1 className='mt-10 mb-8'>Shop Page</h1>
+      <h1 className='mb-8 md:mt-10'>Shop Page</h1>
 
-      <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
-        <div>
-          <Link href='/shop/1'>
-            <Image
-              src='/products/classic-1.webp'
-              width={350}
-              height={350}
-              alt='product-image'
-              className='mb-2 object-cover'
-            />
-            <p className='mb-1 text-center text-lg'>Corsa Classic White Navy</p>
-            <p className='text-center'>Rp 428.000,-</p>
-          </Link>
-        </div>
+      <div className='grid grid-cols-2 gap-6 md:grid-cols-4'>
+        {allProducts?.map((product) => (
+          <div>
+            <Link href={`/products/${product.slug}`}>
+              <div className='relative'>
+                <Image
+                  src={product.thumbnail}
+                  width={350}
+                  height={350}
+                  alt={product.name}
+                  className='mb-2 object-cover'
+                />
+                {product.hot && (
+                  <Badge className='absolute right-2 top-2 text-[8px] leading-normal lg:text-xs'>
+                    HOT
+                  </Badge>
+                )}
+              </div>
+              <p className='mb-1 text-center text-sm lg:text-lg'>
+                {product.name}
+              </p>
+              <p className='text-xs text-center'>{product.price}</p>
+            </Link>
+          </div>
+        ))}
       </div>
     </>
   )
--- data/data.ts
+++ data/data.ts
@@ -1,32 +1,78 @@
-const featuredProduct = [
-  {
-    id: '',
-    name: '',
-    slug: '',
-    hot: '',
-    featured: '',
-    description: '',
-    thumbnail: '',
-    gallery: [''],
-  },
-]
-
-const allProducts = [
+export const allProducts = [
   {
     id: 1,
-    name: 'CORSA CLASSIC WHITE NAVY',
+    name: 'Corsa Classic White Navy',
     slug: 'corsa-classic-white-navy',
     hot: true,
-    featured: false,
+    featured: true,
+    price: 'Rp 428.000,-',
     description:
       'Menjadi sepatu “lifestyle” yang memberikan kesan klasik dengan menggunakan kombinasi material yang breathable dengan semi suede yang bikin adem dan outsole barunya dengan teknologi phylon rubber yang ringan, kenyal dan empuk namun tetap anti slip saat digunakan membuat sneakers ini sangat nyaman dan enak dipakai seharian.',
-    thumbnail: '/products/classic-1',
+    thumbnail: '/products/classic-1.webp',
     gallery: [
-      '/products/classic-1',
-      '/products/classic-2',
-      '/products/classic-3',
-      '/products/classic-4',
-      '/products/classic-5',
+      '/products/classic-1.webp',
+      '/products/classic-2.webp',
+      '/products/classic-3.webp',
+      '/products/classic-4.webp',
+      '/products/classic-5.webp',
+    ],
+  },
+  {
+    id: 2,
+    name: 'Corsa Classic Black White',
+    slug: 'corsa-classic-black-white',
+    hot: false,
+    featured: true,
+    price: 'Rp 398.000,-',
+    description:
+      'Menjadi sepatu “lifestyle” yang memberikan kesan klasik dengan menggunakan kombinasi material yang breathable dengan semi suede yang bikin adem dan outsole barunya dengan teknologi phylon rubber yang ringan, kenyal dan empuk namun tetap anti slip saat digunakan membuat sneakers ini sangat nyaman dan enak dipakai seharian.',
+    thumbnail: '/products/corsa-1.webp',
+    gallery: [
+      '/products/corsa-1.webp',
+      '/products/corsa-2.webp',
+      '/products/corsa-3.webp',
+      '/products/corsa-4.webp',
+      '/products/corsa-5.webp',
+    ],
+  },
+  {
+    id: 3,
+    name: 'Corte Mid Full Black',
+    slug: 'corte-mid-full-black',
+    hot: true,
+    featured: true,
+    price: 'Rp 297.000,-',
+    description:
+      'Menjadi sepatu “lifestyle” yang memberikan kesan klasik dengan menggunakan kombinasi material yang breathable dengan semi suede yang bikin adem dan outsole barunya dengan teknologi phylon rubber yang ringan, kenyal dan empuk namun tetap anti slip saat digunakan membuat sneakers ini sangat nyaman dan enak dipakai seharian.',
+    thumbnail: '/products/corte-1.webp',
+    gallery: [
+      '/products/corte-1.webp',
+      '/products/corte-2.webp',
+      '/products/corte-3.webp',
+      '/products/corte-4.webp',
+      '/products/corte-5.webp',
+    ],
+  },
+  {
+    id: 4,
+    name: 'Corte Jog Full White GS',
+    slug: 'corte-jog-full-white-gs',
+    hot: false,
+    featured: true,
+    price: 'Rp 375.000,-',
+    description:
+      'Menjadi sepatu “lifestyle” yang memberikan kesan klasik dengan menggunakan kombinasi material yang breathable dengan semi suede yang bikin adem dan outsole barunya dengan teknologi phylon rubber yang ringan, kenyal dan empuk namun tetap anti slip saat digunakan membuat sneakers ini sangat nyaman dan enak dipakai seharian.',
+    thumbnail: '/products/white-1.webp',
+    gallery: [
+      '/products/white-1.webp',
+      '/products/white-2.webp',
+      '/products/white-3.webp',
+      '/products/white-4.webp',
+      '/products/white-5.webp',
     ],
   },
 ]
+
+export function featuredProducts() {
+  return allProducts.filter((product) => product.featured)
+}
