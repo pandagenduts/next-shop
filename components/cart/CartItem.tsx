13554ff15056d9cb74a717d6c5dad6901dd223ae
Donny <donny.rendi@yahoo.co.id>
2023-12-29 16:10:39 +0700

    build: cart item a tag to Link

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,17 +1,18 @@
 import { Minus, Plus } from 'lucide-react'
 import { Button } from '../ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function CartItem() {
   return (
     <div className='flex gap-4'>
       <div className='max-w-[90px]'>
-        <a href='/'>
+        <Link href='/'>
           <Image src='/product-1.webp' width={90} height={90} alt='product-img' className='object-cover' />
-        </a>
+        </Link>
       </div>
       <div className='flex flex-1 flex-col justify-between'>
-        <a href='/' className='font-bold'>Product Title</a>
+        <Link href='/' className='font-bold'>Corte Mid Full Black</Link>
         <div className='flex justify-between gap-4'>
           <div className='flex items-center justify-between max-w-[80px] w-full'>
             <Button className='cursor-pointer p-[6px] h-auto' variant='outline'>
