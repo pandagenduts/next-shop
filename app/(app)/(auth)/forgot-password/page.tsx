b941204a9f984761d2fd85bd637f7564aeb75be0
Donny <donny.rendi@yahoo.co.id>
2024-01-17 16:27:57 +0700

    build: forgot-password page

--- app/(app)/(auth)/forgot-password/page.tsx
+++ app/(app)/(auth)/forgot-password/page.tsx
@@ -6,7 +6,28 @@ import Link from 'next/link'
 import { FormEventHandler } from 'react'
 
 export default function page() {
+  const handleForgotPassword: FormEventHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>forgot password</div>
+    <div className='mx-auto mt-20 max-w-md'>
+    <section className='mb-10 flex flex-col items-center'>
+      <h2>Reset password</h2>
+    </section>
+
+    <section>
+      <form onSubmit={handleForgotPassword}>
+        <div className='mb-6'>
+          <label htmlFor='email' className='mb-2 block font-medium'>
+            Email address
+          </label>
+          <Input type='email' id='email' placeholder='Email' required />
+        </div>
+
+        <Button className='mt-6 w-full'>Send password reset link</Button>
+      </form>
+    </section>
+  </div>
   )
 }
