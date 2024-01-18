b07da0f2d9c86101128469bad71afa56ed0ea455
Donny <donny.rendi@yahoo.co.id>
2024-01-18 11:57:28 +0700

    build: integrate forgot password with firebase

--- app/(app)/(auth)/forgot-password/page.tsx
+++ app/(app)/(auth)/forgot-password/page.tsx
@@ -1,33 +1,65 @@
 'use client'
 
+import { auth } from '@/app/firebase'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import Link from 'next/link'
-import { FormEventHandler } from 'react'
+import { sendPasswordResetEmail } from 'firebase/auth'
+import { FormEventHandler, useState } from 'react'
 
 export default function Page() {
+  const [email, setEmail] = useState('')
+  const [errorStatus, setErrorStatus] = useState('')
+  const [success, setSuccess] = useState(false)
+
   const handleForgotPassword: FormEventHandler = (e) => {
     e.preventDefault()
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log('Password reset email sent!')
+        setErrorStatus('')
+        setSuccess(true)
+      })
+      .catch((error) => {
+        console.log(error)
+        setErrorStatus(error.code)
+      })
   }
 
   return (
     <div className='mx-auto mt-20 max-w-md'>
-    <section className='mb-10 flex flex-col items-center'>
-      <h2>Reset password</h2>
-    </section>
+      <section className='mb-10 flex flex-col items-center'>
+        <h2>Reset password</h2>
+      </section>
 
-    <section>
-      <form onSubmit={handleForgotPassword}>
-        <div className='mb-6'>
-          <label htmlFor='email' className='mb-2 block font-medium'>
-            Email address
-          </label>
-          <Input type='email' id='email' placeholder='Email' required />
-        </div>
+      <section>
+        <form onSubmit={handleForgotPassword}>
+          <div className='mb-6'>
+            <label htmlFor='email' className='mb-2 block font-medium'>
+              Email address
+            </label>
+            <Input
+              type='email'
+              id='email'
+              placeholder='Email'
+              required
+              value={email}
+              onChange={(e) => {
+                setErrorStatus('')
+                setEmail(e.target.value)
+              }}
+            />
+          </div>
 
-        <Button className='mt-6 w-full'>Send password reset link</Button>
-      </form>
-    </section>
-  </div>
+          {errorStatus && <p className='mb-4 text-center text-destructive'>{errorStatus}</p>}
+          {success && (
+            <p className='mb-4 text-center'>Password reset link has been sent to your email.</p>
+          )}
+          <Button className='w-full' disabled={email == ''}>
+            Send password reset link
+          </Button>
+        </form>
+      </section>
+    </div>
   )
 }
