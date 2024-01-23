11a0feae80399eadaf750f11f6d81baff74b7395
Donny <donny.rendi@yahoo.co.id>
2024-01-23 20:07:33 +0700

    chore: remove unused things on profile page

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -4,7 +4,6 @@ import { Button } from '@/components/ui/button'
 import useCartStore from '@/store/cart-store'
 import { DocumentReference } from 'firebase/firestore/lite'
 import ky from 'ky'
-import { useEffect } from 'react'
 
 type Token = {
   token: string
@@ -13,44 +12,6 @@ type Token = {
 
 export default function Page() {
   const { cartItemsStore } = useCartStore()
-
-  const handleGetProduct = async () => {
-    const token: Token = await ky
-      .post(`/api/midtrans/generate-token`, { json: cartItemsStore })
-      .json()
-    console.log(token.token)
-
-    window.snap.embed(token.token, {
-      embedId: 'snap-container',
-      onSuccess: function (result: any) {
-        alert('payment success!')
-        console.log(result)
-      },
-      onPending: function (result: any) {
-        alert('wating your payment!')
-        console.log(result)
-      },
-      onError: function (result: any) {
-        alert('payment failed!')
-        console.log(result)
-      },
-      onClose: function () {
-        alert('you closed the popup without finishing the payment')
-      },
-    })
-  }
-
-  useEffect(() => {
-    // inject Midtrans Snap script
-    const script = document.createElement('script')
-    script.src = 'https://app.sandbox.midtrans.com/snap/snap.js'
-    script.setAttribute('data-client-key', process.env.NEXT_PUBLIC_MIDTRANS_CLIENT as string)
-    document.body.appendChild(script)
-
-    return () => {
-      document.body.removeChild(script)
-    }
-  }, [])
 
   const handleCreateDocument = async () => {
     try {
@@ -67,12 +28,11 @@ export default function Page() {
   return (
     <>
       <h4>Profile</h4>
-      <button onClick={handleGetProduct} className='bg-black p-4 text-white'>
-        Get Product with cart-store
-      </button>
+      {/* <p className='mt-8'>Coming soon! ^^</p> */}
 
-      <Button onClick={handleCreateDocument}>Create Document on Backend</Button>
-      <div id='snap-container' className='w-full'></div>
+      <Button className='mt-8' onClick={handleCreateDocument}>
+        Create Order
+      </Button>
     </>
   )
 }
