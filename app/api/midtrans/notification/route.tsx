9a669f07c3194fd11d076f87fe112d6a00fa2694
Donny <donny.rendi@yahoo.co.id>
2024-01-25 16:37:58 +0700

    chore: remove unused const on api midtrans

--- app/api/midtrans/notification/route.tsx
+++ app/api/midtrans/notification/route.tsx
@@ -1,7 +1,7 @@
+const midtransClient = require('midtrans-client')
+
 import { UpdateOrder } from '@/lib/actions/firestore/update-order'
 import { NextResponse } from 'next/server'
-
-const midtransClient = require('midtrans-client')
 
 export async function POST(req: Request) {
   const body = await req.json()
@@ -13,11 +13,8 @@ export async function POST(req: Request) {
     clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT,
   })
 
-  const successResponse = NextResponse.json({ success: true }, { status: 200 })
-  const failResponse = NextResponse.json({ success: false }, { status: 500 })
-  
   try {
-    const statusResponse = await apiClient.transaction.notification(notificationJson);
+    const statusResponse = await apiClient.transaction.notification(notificationJson)
     let orderId = statusResponse.order_id
     let transactionStatus = statusResponse.transaction_status
     let fraudStatus = statusResponse.fraud_status
@@ -30,24 +27,24 @@ export async function POST(req: Request) {
     // Sample transactionStatus handling logic
     if (transactionStatus == 'settlement') {
       // TODO set transaction status on your database to 'success'
-      await UpdateOrder(uid, orderId, { payment_status: 'success' });
-      return NextResponse.json({ success: true }, { status: 200 });
+      await UpdateOrder(uid, orderId, { payment_status: 'success' })
+      return NextResponse.json({ success: true }, { status: 200 })
     } else if (transactionStatus == 'deny') {
       // TODO you can ignore 'deny'
-      return NextResponse.json({ success: true }, { status: 200 });
+      return NextResponse.json({ success: true }, { status: 200 })
     } else if (transactionStatus == 'cancel' || transactionStatus == 'expire') {
       // TODO set transaction status on your database to 'failure'
-      await UpdateOrder(uid, orderId, { payment_status: 'failure' });
-      return NextResponse.json({ success: true }, { status: 200 });
+      await UpdateOrder(uid, orderId, { payment_status: 'failure' })
+      return NextResponse.json({ success: true }, { status: 200 })
     } else if (transactionStatus == 'pending') {
       // TODO set transaction status on your database to 'pending' / waiting payment
-      await UpdateOrder(uid, orderId, { payment_status: 'pending' });
-      return NextResponse.json({ success: true }, { status: 200 });
+      await UpdateOrder(uid, orderId, { payment_status: 'pending' })
+      return NextResponse.json({ success: true }, { status: 200 })
     }
 
-    return NextResponse.json({ success: true }, { status: 200 });
+    return NextResponse.json({ success: true }, { status: 200 })
   } catch (error) {
-    console.log(error);
-    return NextResponse.json({ success: false }, { status: 500 });
+    console.log(error)
+    return NextResponse.json({ success: false }, { status: 500 })
   }
 }
